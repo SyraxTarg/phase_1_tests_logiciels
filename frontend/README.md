78aa4a0c20437f9473a329e351cb9e0443cd0bb6
Claire Sbaffe <claire.sbaffe@efrei.net>
2026-03-17 10:19:49 +0100

    FEAT add URL_PAGE to env

--- frontend/README.md
+++ frontend/README.md
@@ -34,3 +34,17 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+# Playwright
+
+Make sure your backend is launched and properly connected to frontend.
+
+Run Playwright tests with command :
+```bash
+npx playwright test
+```
+
+If tests fail, you might want to download test browsers :
+```bash
+npx playwright install
+```
--- frontend/tests/home.spec.ts
+++ frontend/tests/home.spec.ts
@@ -1,26 +1,31 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import 'dotenv/config';
 
-const URL_PAGE = 'http://localhost:3000/';
+const URL_PAGE = process.env.URL_PAGE;
 
-test.describe('Page Communauté de Troc', () => {
+if (!URL_PAGE) {
+  throw new Error("L'élément URL_PAGE n'est pas défini dans l'environnement.");
+}
 
+test.describe("Page Communauté de Troc", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(URL_PAGE);
   });
 
-  test('liste les utilisateurs et permet d’accéder à un deck', async ({ page }) => {
-    const userListItems = page.locator('ul > li');
+  test("liste les utilisateurs et permet d’accéder à un deck", async ({
+    page,
+  }) => {
+    const userListItems = page.locator("ul > li");
     const count = await userListItems.count();
 
     if (count > 0) {
       const firstUser = userListItems.first();
 
-      const deckButton = firstUser.getByRole('link', { name: /Voir le Deck/i });
+      const deckButton = firstUser.getByRole("link", { name: /Voir le Deck/i });
       await expect(deckButton).toBeVisible();
 
       await deckButton.click();
       await expect(page).toHaveURL(/\/profile\//);
     }
   });
-
 });
