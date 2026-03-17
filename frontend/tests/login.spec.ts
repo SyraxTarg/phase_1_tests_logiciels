88c414b27b3dc0c992f4e9328a4b5cc16b40b5c4
SyraxTarg <maimiti.saint-marc@efrei.net>
2026-03-17 13:51:21 +0100

    AAA format for login test

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -3,11 +3,19 @@ import { test, expect } from '@playwright/test';
 test('login successful', async ({ page, browserName }) => {
 
   test.skip(browserName === 'webkit', 'Webkit a des problèmes de gestion de session en local');
-  await page.goto('http://localhost:3000/login');
-  await page.getByRole('textbox', { name: 'Username' }).fill('Alice');
-  await page.getByRole('textbox', { name: 'Mot de passe' }).fill('password123');
-  await page.getByRole('button', { name: 'Se connecter' }).click();
 
+  // Arrange
+  await page.goto('http://localhost:3000/login');
+  const username_textbox = await page.getByRole('textbox', { name: 'Username' });
+  const password_textbox = await page.getByRole('textbox', { name: 'Mot de passe' });
+  const submit_button = await page.getByRole('button', { name: 'Se connecter' });
+
+  // Act
+  await username_textbox.fill('Alice');
+  await password_textbox.fill('password123');
+  await submit_button.click();
+
+  // Assert
   await expect(page).toHaveURL('http://localhost:3000/');
 });
 
@@ -15,10 +23,19 @@ test('login successful', async ({ page, browserName }) => {
 test('login failed', async ({ page, browserName }) => {
 
   test.skip(browserName === 'webkit', 'Webkit a des problèmes de gestion de session en local');
+
+  // Arrange
   await page.goto('http://localhost:3000/login');
-  await page.getByRole('textbox', { name: 'Username' }).fill('Alice');
-  await page.getByRole('textbox', { name: 'Mot de passe' }).fill('toto');
-  await page.getByRole('button', { name: 'Se connecter' }).click();
+  const username_textbox = await page.getByRole('textbox', { name: 'Username' });
+  const password_textbox = await page.getByRole('textbox', { name: 'Mot de passe' });
+  const submit_button = await page.getByRole('button', { name: 'Se connecter' });
+
+  // Act
+  await username_textbox.fill('Alice');
+  await password_textbox.fill('toto');
+  await submit_button.click();
+
+  // Assert
   await page.getByText("Identifiants incorrects.")
 });
 
@@ -26,10 +43,16 @@ test('login failed', async ({ page, browserName }) => {
 test('login no username', async ({ page, browserName }) => {
 
   test.skip(browserName === 'webkit', 'Webkit a des problèmes de gestion de session en local');
+
   await page.goto('http://localhost:3000/login');
-  await page.getByRole('textbox', { name: 'Username' }).fill('');
-  await page.getByRole('textbox', { name: 'Mot de passe' }).fill('password123');
-  await page.getByRole('button', { name: 'Se connecter' }).click();
+  const username_textbox = await page.getByRole('textbox', { name: 'Username' });
+  const password_textbox = await page.getByRole('textbox', { name: 'Mot de passe' });
+  const submit_button = await page.getByRole('button', { name: 'Se connecter' });
+
+  await username_textbox.fill('');
+  await password_textbox.fill('password123');
+  await submit_button.click();
+
   await page.getByText("Identifiants incorrects.")
 });
 
@@ -37,9 +60,15 @@ test('login no username', async ({ page, browserName }) => {
 test('login no password', async ({ page, browserName }) => {
 
   test.skip(browserName === 'webkit', 'Webkit a des problèmes de gestion de session en local');
+
   await page.goto('http://localhost:3000/login');
-  await page.getByRole('textbox', { name: 'Username' }).fill('Alice');
-  await page.getByRole('textbox', { name: 'Mot de passe' }).fill('');
-  await page.getByRole('button', { name: 'Se connecter' }).click();
+  const username_textbox = await page.getByRole('textbox', { name: 'Username' });
+  const password_textbox = await page.getByRole('textbox', { name: 'Mot de passe' });
+  const submit_button = await page.getByRole('button', { name: 'Se connecter' });
+
+  await username_textbox.fill('Alice');
+  await password_textbox.fill('');
+  await submit_button.click();
+
   await page.getByText("Identifiants incorrects.")
 });
