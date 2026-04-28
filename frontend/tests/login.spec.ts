0a5105998e49429f0776fbeb7fe01e4ca27ab885
Claire Sbaffe <claire.sbaffe@efrei.net>
2026-04-28 15:05:12 +0200

    FIX login Playwright tests

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -1,5 +1,5 @@
-import { test, expect } from '@playwright/test';
-import 'dotenv/config';
+import { test, expect } from "@playwright/test";
+import "dotenv/config";
 
 const URL_PAGE = process.env.URL_PAGE;
 
@@ -7,83 +7,93 @@ if (!URL_PAGE) {
   throw new Error("L'élément URL_PAGE n'est pas défini dans l'environnement.");
 }
 
-
 test.use({ storageState: { cookies: [], origins: [] } });
-test('login successful', async ({ page, browserName }) => {
-
-  // test.skip(browserName === 'webkit', 'Webkit a des problèmes de gestion de session en local');
-
+test("login successful", async ({ page }) => {
   // Arrange
   await page.goto(`${URL_PAGE}/login`);
-  const username_textbox = await page.getByRole('textbox', { name: 'Username' });
-  const password_textbox = await page.getByRole('textbox', { name: 'Mot de passe' });
-  const submit_button = await page.getByRole('button', { name: 'Se connecter' });
+  const username_textbox = page.getByRole("textbox", { name: "Username" });
+  const password_textbox = page.getByRole("textbox", { name: "Mot de passe" });
+  const submit_button = page.getByRole("button", { name: "Se connecter" });
 
   // Act
-  await username_textbox.fill('Alice');
-  await password_textbox.fill('password123');
+  await username_textbox.fill("Alice");
+  await password_textbox.fill("password123");
   await submit_button.click();
 
   // Assert
   await expect(page).toHaveURL(`${URL_PAGE}/`);
 });
 
-
-test('login failed', async ({ page, browserName }) => {
-
+test("login failed", async ({ page, browserName }) => {
   // test.skip(browserName === 'webkit', 'Webkit a des problèmes de gestion de session en local');
 
   // Arrange
   await page.goto(`${URL_PAGE}/login`);
-  const username_textbox = await page.getByRole('textbox', { name: 'Username' });
-  const password_textbox = await page.getByRole('textbox', { name: 'Mot de passe' });
-  const submit_button = await page.getByRole('button', { name: 'Se connecter' });
+  const username_textbox = await page.getByRole("textbox", {
+    name: "Username",
+  });
+  const password_textbox = await page.getByRole("textbox", {
+    name: "Mot de passe",
+  });
+  const submit_button = await page.getByRole("button", {
+    name: "Se connecter",
+  });
 
   // Act
-  await username_textbox.fill('Alice');
-  await password_textbox.fill('toto');
+  await username_textbox.fill("Alice");
+  await password_textbox.fill("toto");
   await submit_button.click();
 
   // Assert
-  await expect(page.getByText("Identifiants incorrects.")).toBeVisible({ timeout: 7000 });
+  await expect(page.getByText("Identifiants incorrects.")).toBeVisible({
+    timeout: 7000,
+  });
   await expect(page).toHaveURL(`${URL_PAGE}/login`);
 });
 
-
-test('login no username', async ({ page, browserName }) => {
-
+test("login no username", async ({ page, browserName }) => {
   // test.skip(browserName === 'webkit', 'Webkit a des problèmes de gestion de session en local');
 
   // Arrange
   await page.goto(`${URL_PAGE}/login`);
-  const username_textbox = await page.getByRole('textbox', { name: 'Username' });
-  const password_textbox = await page.getByRole('textbox', { name: 'Mot de passe' });
-  const submit_button = await page.getByRole('button', { name: 'Se connecter' });
+  const username_textbox = await page.getByRole("textbox", {
+    name: "Username",
+  });
+  const password_textbox = await page.getByRole("textbox", {
+    name: "Mot de passe",
+  });
+  const submit_button = await page.getByRole("button", {
+    name: "Se connecter",
+  });
 
   // Act
-  await username_textbox.fill('');
-  await password_textbox.fill('password123');
-  await submit_button.click({force: true});
+  await username_textbox.fill("");
+  await password_textbox.fill("password123");
+  await submit_button.click({ force: true });
 
   // Assert
   await expect(page.getByText("Identifiants incorrects.")).toBeHidden();
   await expect(page).toHaveURL(`${URL_PAGE}/login`);
 });
 
-
-test('login no password', async ({ page, browserName }) => {
-
+test("login no password", async ({ page, browserName }) => {
   // test.skip(browserName === 'webkit', 'Webkit a des problèmes de gestion de session en local');
 
   // Arrange
   await page.goto(`${URL_PAGE}/login`);
-  const username_textbox = await page.getByRole('textbox', { name: 'Username' });
-  const password_textbox = await page.getByRole('textbox', { name: 'Mot de passe' });
-  const submit_button = await page.getByRole('button', { name: 'Se connecter' });
+  const username_textbox = await page.getByRole("textbox", {
+    name: "Username",
+  });
+  const password_textbox = await page.getByRole("textbox", {
+    name: "Mot de passe",
+  });
+  const submit_button = await page.getByRole("button", {
+    name: "Se connecter",
+  });
 
   // Act
-  await username_textbox.fill('Alice');
-  await password_textbox.fill('');
+  await username_textbox.fill("Alice");
+  await password_textbox.fill("");
   await submit_button.click();
 
   // Assert
