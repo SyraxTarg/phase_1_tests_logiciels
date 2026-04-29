1ef03546dc18d50ff81b93ce3e63042072aed1e5
SyraxTarg <maimiti.saint-marc@efrei.net>
2026-04-29 11:06:16 +0200

    added healthcheck route

--- back/index.js
+++ back/index.js
@@ -9,14 +9,17 @@ const userRouter = require('./routes/user');
 const cardRouter = require('./routes/card');
 const transactionRouter = require('./routes/transaction');
 const messageRouter = require('./routes/message');
+const healthRouter = require('./routes/health');
 
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/cards', cardRouter);
 app.use('/transactions', transactionRouter);
 app.use('/messages', messageRouter);
+app.use('/health', healthRouter);
 
 const LISTEN_PORT = process.env["LISTEN_PORT"]
+
 
 app.listen(LISTEN_PORT, () => {
   console.log(`Serveur lancé sur http://localhost:${LISTEN_PORT}`);
